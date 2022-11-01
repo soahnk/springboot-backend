04f833c8cb11d58b4021e22fcd10a02cee8e30c7
soahnk <sohankhadka82@gmail.com>
2022-11-01 14:52:41 -0500

    Removed comments

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -27,35 +27,27 @@ public class EmployeeController {
 		this.employeeService = employeeService;
 	}
 	
-	// build create employee REST API
 	@PostMapping()
 	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
 		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
 	}
 	
-	// build get all employees REST API
 	@GetMapping
 	public List<Employee> getAllEmployees(){
 		return employeeService.getAllEmployees();
 	}
 	
-	// build get employee by id REST API
-	// http://localhost:8080/api/employees/1
 	@GetMapping("{id}")
 	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId){
 		return new ResponseEntity<Employee>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
 	}
-	
-	// build update employee REST API
-	// http://localhost:8080/api/employees/1
+
 	@PutMapping("{id}")
 	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id
 												  ,@RequestBody Employee employee){
 		return new ResponseEntity<Employee>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
 	}
 	
-	// build delete employee REST API
-	// http://localhost:8080/api/employees/1
 	@DeleteMapping("{id}")
 	public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
 		
